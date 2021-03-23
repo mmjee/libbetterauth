b06a721979113427ce93436e09074fc4f101f2c7
root <root@git.maharshi.ninja>
2021-03-23 13:13:47 +0530

    Also handle query

--- express.js
+++ express.js
@@ -1,9 +1,15 @@
+
 const { verifyData } = require('./index')
 
 module.exports = function (getUser, pickPublicKey) {
   return async function (req, res, next) {
-    const { body } = req
-    if (body._sig && body.timestamp && body.userID) {
+    let body
+    if (req.body._sig && req.body.timestamp && req.body.userID) {
+      body = req.body
+    } else if (req.query._sig && req.query.timestamp && req.query.userID) {
+      body = req.query
+    }
+    if (body) {
       const user = await getUser(body.userID)
       if (!user) {
         // res.status(401).header('WWW-Authenticate', 'SignedMessage').send()
