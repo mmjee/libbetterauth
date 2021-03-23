88ecdb2d978eef3123dd439ccae600e41cc92325
root <root@git.maharshi.ninja>
2021-03-23 12:48:55 +0530

    Add actual error handling

--- express.js
+++ express.js
@@ -6,14 +6,20 @@ module.exports = function (getUser, pickPublicKey) {
     if (body._sig && body.timestamp && body.userID) {
       const user = await getUser(body.userID)
       if (!user) {
-        // res.status(403).header('WWW-Authenticate', 'SignedMessage').send()
-        res.status(401).send()
+        // res.status(401).header('WWW-Authenticate', 'SignedMessage').send()
+        res.status(401).send({
+          error: true,
+          errorMessage: 'NO_USER_ID'
+        })
         return
       }
       const pubKey = pickPublicKey(user)
       const validated = verifyData(body, pubKey)
       if (!validated) {
-        res.status(401).send()
+        res.status(401).send({
+          error: true,
+          errorMessage: 'VERIFICATION_FAILED'
+        })
         return
       }
       req.user = user
@@ -25,7 +31,10 @@ module.exports = function (getUser, pickPublicKey) {
 
 module.exports.authenticationMandatory = function (req, res, next) {
   if (!req.user) {
-    res.status(403).send()
+    res.status(403).send({
+      error: true,
+      errorMessage: 'AUTHENTICATION_MANDATORY'
+    })
   } else {
     next()
   }
